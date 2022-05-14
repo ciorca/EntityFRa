28fc7a5b4aa3cfd51389b75ceda996fa0a855f56
ciorca <ciorca.gaby@gmail.com>
2022-05-14 19:00:42 +0300

    Add project files.

---
+++ EntityFRa/EmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityFRa
+{
+    public class EmployeeRepository
+    {
+
+        public List<Department>GetDepartments()
+        {
+
+            EmployeeDBContext employeeDBContext = new EmployeeDBContext();
+            return employeeDBContext.Departments.ToList();
+        }
+    }
+}
